e5b8820042e12c9788a96d47713847c4ca19843a
Sweetcandy273 <sweetcandy.aum@gmail.com>
2020-09-20 22:11:38 +0700

    adjust detail

--- js/app.js
+++ js/app.js
@@ -5,6 +5,13 @@ document.addEventListener('init', function (event) {
         page.querySelector('#john_wick_1').onclick = function () {
             document.querySelector('#myNavigator').pushPage('views/john_wick_1.html', { data: { title: 'John Wick 1' } });
         };
+        page.querySelector('#john_wick_2').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_2.html', { data: { title: 'John Wick 1' } });
+        };
+        page.querySelector('#john_wick_3').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_3.html', { data: { title: 'John Wick 1' } });
+        };
+        
     } else if (page.id === 'page2') {
         page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
     }
