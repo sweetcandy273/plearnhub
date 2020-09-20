eea37bcf8006975b8e92ff7816202994dd666b30
Sweetcandy273 <sweetcandy.aum@gmail.com>
2020-09-20 19:35:27 +0700

    add detail johnwick1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-document.addEventListener('init', function(event) {
+document.addEventListener('init', function (event) {
     var page = event.target;
-  
+
     if (page.id === 'page1') {
-      page.querySelector('#push-button').onclick = function() {
-        document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-      };
+        page.querySelector('#john_wick_1').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/john_wick_1.html', { data: { title: 'John Wick 1' } });
+        };
     } else if (page.id === 'page2') {
-      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
     }
-  });
+});
